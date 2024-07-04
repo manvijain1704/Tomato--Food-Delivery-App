486baf0aaca18ce797d79be57af9b31af98f021d
Manvi Jain <manvijain1704@gmail.com>
2024-07-03 22:50:33 -0400

    initial commit

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Introducing Tomato, the ultimate food delivery app designed to bring your favorite meals right to your doorstep. Whether you're craving a gourmet burger, a fresh sushi platter, or a comforting bowl of pasta, Tomato has you covered with an extensive range of local restaurants and cuisines.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const placeorder = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default placeorder
